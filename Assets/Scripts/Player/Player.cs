6a4636cd5d9d58d1ef96df00970eab3c31d2f7c4
saryannec <128328709+saryannec@users.noreply.github.com>
2025-07-09 16:57:26 -0300

    add gameplay

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Player : MonoBehaviour
+{
+
+  public Rigidbody2D myRigidbody;
+  [Header("Speed setup")]
+
+  public Vector2 friction = new Vector2(.1f, 0);
+
+  public float speed;
+  public float speedRun;
+  public float forceJump;
+
+  [Header("Animation setup")]
+  public float jumpScaleY = 1.5f;
+  public float jumpScaleX = 0.7f;
+  public float animationDuration = .3f;
+  public Ease ease = Ease.OutBack;
+
+  private float _currentSpeed;
+
+  private void Update(){
+        HandleMoviment();
+        HandleJump();
+  }
+
+  private void HandleMoviment(){
+
+    if (Input.GetKey(KeyCode.LeftControl))
+       _currentSpeed = speedRun;
+    else
+       _currentSpeed = speed;
+
+
+    if(Input.GetKey(KeyCode.LeftArrow)){
+        myRigidbody.velocity = new Vector2(-_currentSpeed, myRigidbody.velocity.y);
+    }
+
+    else if(Input.GetKey(KeyCode.RightArrow)){
+        myRigidbody.velocity = new Vector2(_currentSpeed, myRigidbody.velocity.y);
+    }
+
+    if(myRigidbody.velocity.x > 0){
+        myRigidbody.velocity += friction;
+    }
+    else if (myRigidbody.velocity.x < 0){
+        myRigidbody.velocity -= friction;
+    }
+  }
+
+  private void HandleJump(){
+        if (Input.GetKeyDown(KeyCode.Space)){
+        myRigidbody.velocity = Vector2.up * forceJump;
+        myRigidbody.transform.localScale = Vector2.one;
+        DOTween.Kill(myRigidbody.transform);
+        HandleScaleJump();
+    }      
+  }
+
+  private void HandleScaleJump(){
+        myRigidbody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidbody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+
+  }
+
+}
